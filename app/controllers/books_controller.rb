f027cb39dc6804b7d26be8e692203132537450e1
diegoarvz4 <darvz.gtz@gmail.com>
2020-02-10 23:08:05 -0600

    API CRUD added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,42 @@
 class BooksController < ApplicationController
-
+  before_action :set_book, only: [:show, :update, :destroy]
 
   def index 
     @books = Book.all
+    json_response(@books)
+  end
+
+  def show
     json_response(@book)
   end
 
+  def create 
+    @book = Book.new(book_params)
+    if @book.save
+      json_response(@book)
+    end
+  end
+
+  def update
+    @book.update_attributes(book_params)
+    if @book.save
+      json_response(@book)
+    end
+    head :no_content
+  end
+
+  def destroy
+    @book.destroy
+    head :no_content
+  end
+
+  private
+
+    def book_params
+      params.permit(:title, :category)
+    end
+
+    def set_book
+      @book = Book.find_by(id: params[:id])
+    end
 end
--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -3,12 +3,12 @@ module ExceptionHandler
 
   included do
     rescue_from ActiveRecord::RecordNotFound do |e|
-      json_response({message: e.message}, :not_found)
+      json_response({ message: e.message }, :not_found)
     end
 
-    rescue_from ActiveRecord::RecordInvalid do |e| 
-      json_response({message: e.message}, :unprocessable_entity)
+    rescue_from ActiveRecord::RecordInvalid do |e|
+      json_response({ message: e.message }, :unprocessable_entity)
     end
   end
-  
+
 end
