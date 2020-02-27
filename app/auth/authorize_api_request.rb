48022b49806105b2f9730436e4f5ffeca3cf8473
diegoarvz4 <darvz.gtz@gmail.com>
2020-02-26 21:53:52 -0600

    Added AuthorizeApiRequest.rb and Message.rb

---
+++ app/auth/authorize_api_request.rb
@@ -0,0 +1,37 @@
+class AuthorizeApiRequest
+
+  def initialize(headers = {})
+    @headers = headers
+  end
+
+  def call 
+    {
+      user: user
+    }
+  end
+
+  private 
+
+  def user
+    @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
+  rescue ActiveRecord::RecortNotFound => e 
+
+    raise(
+      ExceptionHandler::InvalidToken,
+      ("#{Message.invalid_token} #{e.message}")
+    )
+
+  end
+
+  def decoded_auth_token
+    @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
+  end
+
+  def http_auth_header
+    if(headers['Authorization']).present?
+      return headers['Authorization'].split(' ').last
+    end
+    raise(ExceptionHandler::MissingToken, Message.missing_token)
+  end
+
+end
